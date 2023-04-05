77538ed4a7945c3f4ceec56d3e224a5c387ed852
andrew-braun <andrew@andrewbraun.dev>
2023-04-05 22:50:48 +0400

    feat: Add Twitter sharing link

--- src/components/share/ShareButtons.svelte
+++ src/components/share/ShareButtons.svelte
@@ -2,6 +2,14 @@
 	import { page } from "$app/stores";
 	import LinkBold from "~icons/ph/link-bold";
 	import FacebookLogo from "~icons/ph/facebook-logo";
+	import TwitterLogo from "~icons/ph/twitter-logo";
+	import { visited } from "stores/countries";
+	import type { CountryId } from "ts/countries";
+
+	let visitedCountries: CountryId[] = [];
+	visited.subscribe((countries) => {
+		visitedCountries = countries;
+	});
 
 	let currentUrl = "";
 	page.subscribe((page) => (currentUrl = page.url.toString()));
@@ -13,13 +21,22 @@
 </script>
 
 <div class="container">
-	<button class="share-button copy-url" on:click={(event) => handleCopyUrlClick(event)}
+	<button class="share-button share-link copy-url" on:click={(event) => handleCopyUrlClick(event)}
 		><LinkBold /></button
 	>
 	<a
 		href={`https://www.facebook.com/share.php?u=${currentUrl}`}
 		class="share-link facebook"
 		target="_blank"><FacebookLogo /></a
+	>
+	<a
+		href={`https://twitter.com/intent/tweet?url=${encodeURIComponent(
+			currentUrl
+		)}?text=${encodeURIComponent(
+			`I've been to ${visitedCountries.length} countries! How about you?`
+		)}`}
+		class="share-link twitter"
+		target="_blank"><TwitterLogo /></a
 	>
 </div>
 
@@ -30,9 +47,10 @@
 		padding: var(--spacing-sm) 0;
 
 		.share-button {
-			&.copy-url {
-				margin-right: var(--spacing-sm);
-			}
+			background: none;
+			border: none;
+			padding: 0;
+			margin: 0;
 		}
 	}
 </style>
